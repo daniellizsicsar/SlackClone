3cfc1fcd3244c541d4ffef1cf2795e93d6882b2a
Daniel Lizsicsar <daniel.lizsicsar@gmail.com>
2018-01-04 00:27:42 +0100

    sidemenu UI kind of done

---
+++ Smack/View/GradientView.swift
@@ -0,0 +1,34 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Daniel Lizsicsar on 03/01/2018.
+//  Copyright © 2018 Daniel Lizsicsar. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.1617248342, green: 0.2660157062, blue: 0.4272182642, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.6625445272, green: 0.369877269, blue: 0.373644791, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+        }
+}
